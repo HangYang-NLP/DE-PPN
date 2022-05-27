b2bc8134db304a7d24ca470f0136426f54a52294
yh <78793466+HangYang-NLP@users.noreply.github.com>
2022-05-27 10:34:37 +0800

    Add files via upload

--- run_multi.sh
+++ run_multi.sh
@@ -7,16 +7,16 @@ NUM_GPUS=2
 
 {
     CUDA_VISIBLE_DEVICES=${CUDA} bash train_multi.sh ${NUM_GPUS} \
-        --task_name='SetPre4DEE' \
+        --task_name='SetPre4DEE_new' \
         --use_bert=False \
         --skip_train=False \
         --train_nopair_sets=True  \
         --start_epoch=0 \
         --num_train_epochs=100 \
         --train_batch_size=32 \
-        --gradient_accumulation_steps=8 \
-        --learning_rate=0.0002 \
-        --decoder_lr=0.0001 \
+        --gradient_accumulation_steps=4 \
+        --learning_rate=1e-4 \
+        --decoder_lr=2e-5 \
         --train_file_name='train.json' \
         --dev_file_name='dev.json' \
         --test_file_name='test.json' \
